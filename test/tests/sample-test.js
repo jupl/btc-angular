e8ee0f6ea03784742c598f098152f354ba51821e
Juan Placencia <juan.placencia.512@gmail.com>
2013-02-17 04:32:11 -0600

    Changed sample test to use expect instead of should

--- test/tests/sample-test.js
+++ test/tests/sample-test.js
@@ -7,21 +7,15 @@ describe('An array', function() {
     });
 
     it('should exist', function() {
-      should.exist(array);
+      expect(array).to.exist;
     });
 
     it('should not be null', function() {
-      array.should.not.be["null"];
+      expect(array).to.not.be.null;
     });
 
     it('should have a length of 0', function() {
-      array.length.should.equal(0);
-    });
-    
-    it('should return -1 when looking for index of an item in array', function() {
-      array.indexOf(-1).should.equal(-1);
-      array.indexOf(0).should.equal(-1);
-      array.indexOf(1).should.equal(-1);
+      expect(array).to.have.length(0);
     });
   });
 });
