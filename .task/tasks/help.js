81f2f76a39b15e9a439ef4a11ba6f0364c1e17d7
Juan Placencia <juan.placencia.512@gmail.com>
2013-09-22 17:38:50 -0500

    Bugfix for listing tasks

--- .task/tasks/help.js
+++ .task/tasks/help.js
@@ -6,28 +6,29 @@ var help = module.exports = Object.create(null);
 help.maxCommandLength = 24;
 
 help.npm = function() {
-  var tasks = require('../tasks');
+  var tasks = require('..');
   
   console.log([
     'There are two ways to run tasks. You can use the cake command if you have CoffeeScript installed. (see http://coffeescript.org/) You can also use npm to execute commands:',
     '',
     '  npm run-script [command]',
     '',
-    'Generator commands (gen:) will require a prompt. Command list:'
+    'Generator commands (gen:) will require a prompt. Command list:',
+    ''
   ].join('\n'));
   this.printTasks(tasks);
 };
 
 help.printTasks = function(tasks) {
-  if(typeof taskObject !== 'object') {
+  if(typeof tasks !== 'object') {
     return;
   }
 
-  if(tasks.command && tasks.description & tasks.task) {
+  if(tasks.command && tasks.description && tasks.task) {
     var command = tasks.command
     .padRight(' ', this.maxCommandLength)
     .truncate(this.maxCommandLength, true, 'right', '');
-    console.log(command + ' # ' + taskks.description);
+    console.log(command + ' # ' + tasks.description);
   }
   else {
     for(var key in tasks) {
