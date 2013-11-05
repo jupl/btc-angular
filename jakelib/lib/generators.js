961b7f084cf1fdb9c7d1b0b7e3647ff221d55311
Juan Placencia <juan.placencia.512@gmail.com>
2013-11-05 11:16:14 -0600

    Document generators helper in jakelib

--- jakelib/lib/generators.js
+++ jakelib/lib/generators.js
@@ -3,6 +3,19 @@ var fs = require('fs');
 var jsonfile = require('jsonfile');
 var path = require('path');
 
+/**
+ * List of available generators from Scaffolt. Each element has the following
+ * properties:
+ *   name         Generator name that is to be passed to Scaffolt
+ *   task         Same as name, but its name is formatted to be friendly with
+ *                Jake task names
+ *   description  Description of generator. If one is not defined in Scaffolt,
+ *                then make an educated guess with the name.
+ *   isModule     If true, then when generating the scaffold the name parameter
+ *                is ignored. Otherwise, the name parameter is used for
+ *                scaffolding.
+ * @type {Array}
+ */
 module.exports = fs.readdirSync('generators').filter(function(generator) {
   return fs.existsSync(path.resolve('generators', generator, 'generator.json'));
 })
