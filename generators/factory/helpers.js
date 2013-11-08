f340eac0b7dac573a06e5e633a98fcd1b8e0c05d
Juan Placencia <juan.placencia.512@gmail.com>
2013-11-08 13:39:24 -0600

    Add more generators and clean up existing

--- generators/factory/helpers.js
+++ generators/factory/helpers.js
@@ -1,10 +1,6 @@
 require('sugar');
 
 module.exports = function(Handlebars) {
-  Handlebars.registerHelper('humanize', function(options) {
-    return new Handlebars.SafeString(options.fn(this).humanize());
-  });
-
   Handlebars.registerHelper('dasherize', function(options) {
     return new Handlebars.SafeString(options.fn(this).dasherize());
   });
