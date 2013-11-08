044c92c0402a98edcbff6f02b0e529589eb6fb03
Juan Placencia <juan.placencia.512@gmail.com>
2013-11-08 13:47:14 -0600

    Clean up sample code

--- app/app.js
+++ app/app.js
@@ -1,9 +1,15 @@
 angular.module('app').config([
   '$routeProvider',
   '$locationProvider',
-  function($routeProvider, $locationProvider, config) {
+  function($routeProvider, $locationProvider) {
     // Set up router
     $routeProvider.otherwise({templateUrl: 'partials/index'});
     $locationProvider.html5Mode(true);
+  }
+])
+.run([
+  '$rootScope',
+  function($rootScope) {
+    $rootScope.title = 'Aang Brunch';
   }
 ]);
