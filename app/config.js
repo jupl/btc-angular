ba206dc5e2ff9ba1f855fba3e75812c526f39563
Juan Placencia <juan.placencia.512@gmail.com>
2013-11-15 01:11:26 -0600

    Redefine factory in skeleton, and add provider generator

--- app/config.js
+++ app/config.js
@@ -1,12 +1,10 @@
 angular.module('app.controllers', []);
 angular.module('app.directives', []);
-angular.module('app.factories', []);
 angular.module('app.filters', []);
 angular.module('app.services', []);
 angular.module('app', [
   'app.controllers',
   'app.directives',
-  'app.factories',
   'app.filters',
   'app.services',
   'ngRoute'
